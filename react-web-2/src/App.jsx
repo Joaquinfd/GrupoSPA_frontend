215f43f6760bc8737ecf385f92627a572864c3b3
joaquinfd <jffuentealba@uc.cl>
2023-09-07 16:25:55 -0300

    React-web-2

---
+++ react-web-2/src/App.jsx
@@ -0,0 +1,46 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Navbar from "./components/Navbar";
+
+
+
+function App() {
+
+  const abc = 'abcdefghijklmnñopqrstuvwxyz'
+  const [letter, setAbc] = useState(0);
+
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+
+      <div>
+        <Navbar />
+        
+      </div>
+
+      <div className='card-container'> 
+        
+        <div className="card">
+          <h1>N°</h1>
+          <h2>{count}</h2>
+          <button onClick={() => setCount((count) => count + 1)}>
+            click
+          </button>
+        </div>
+
+        <div className='card'>
+          <h1>ABC</h1>
+          <h2>{abc[letter]}</h2>
+          <button onClick={() => {if (letter < 26) {setAbc((letter) => letter + 1)}}}>
+            click
+          </button>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ react-web-2/src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+// Navbar.js 
+export default function Navbar() {
+    return (
+      <nav className="navigation">
+        <a href="/" className="boton-nav-inicio">
+          foto-logo-inicio
+        </a>
+        <a href="/" className="boton-nav">
+          Objetivo
+        </a>
+        <a href="/" className="boton-nav">
+          Sobre nosotros
+        </a>
+          
+        
+        
+      </nav>
+  );
+}
