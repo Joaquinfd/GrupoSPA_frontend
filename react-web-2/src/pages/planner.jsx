64d474c3476f9115fa46c98c7bfe341dca415b5b
joaquinfd <jffuentealba@uc.cl>
2023-09-11 00:16:44 -0300

    Calendario primeros pasos

--- react-web-2/src/pages/planner.jsx
+++ react-web-2/src/pages/planner.jsx
@@ -1,12 +1,15 @@
 import React, { useState } from 'react';
+import Calendar from 'react-calendar';
 
 
 function Planner() {
 
     return (
-        <p>
-        Calendario con las planificaciones. Se podrá crear y editar rutinas
-        </p>
+        <div>
+            <Calendar/>
+            {/* Añadir function para mostrar dia actual 
+            Inspeccionar e ir cambiando el css cada clase, cuadros de dias mas grandes, botones de añadir actividad, etc.*/}
+        </div>
 
     );  
 }
