895773a45a94144c37bf8ca12ac069ce3d7d134c
joaquinfd <jffuentealba@uc.cl>
2023-09-14 00:17:01 -0300

    creacion id's home

--- react-web-2/src/pages/home.jsx
+++ react-web-2/src/pages/home.jsx
@@ -2,13 +2,10 @@ import React, { useState } from 'react';
 import './home.css'
 import { Carousel } from 'react-responsive-carousel';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-
-
-
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 
 export default function Home() {
-
 
     // const images = ["images/home-image-1.jpg", 
     // "images/home-image-2.jpg", "images/home-image-3.jpg"];
@@ -29,11 +26,11 @@ export default function Home() {
     ];
 
     return (
-        <div className="home-main-container">
-            <div className="home-container">
+        <div className="home-main-container" id='home-div-hmc'>
+            <div className="home-container" id='home-div-hc'>
                 <h1>Mensaje bienvenida</h1>
-                <div className="presentation-container">
-                    <Carousel useKeyboardArrows={false} className='carousel'>
+                <div className="presentation-container" id='home-div-pc'>
+                    <Carousel useKeyboardArrows={false} className='carousel' id='home-carousel'>
                         {images.map((imageData, index) => (
                         <div className="slide" key={index}>
                             <img alt="image-container" src={imageData.url} />
@@ -43,9 +40,12 @@ export default function Home() {
                         ))}
                         {/* src: https://cloudinary.com/blog/add-a-responsive-image-carousel-to-your-react-app */}
                     </Carousel>
-                    <button className='boton-registro'>
-                        Registrate
-                    </button>
+                    <Link to="/ingreso">
+                        <button className='boton-registro' id='home-button-br'>
+                            <h2>Inicia sesión / Registrate</h2>
+                        </button>
+                    </Link>
+                    
                 </div>
             </div>
         </div>
